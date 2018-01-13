38f14102cd7e0d271e873aa8a918877db126718f
Agam Dua <agam@comfylabs.io>
2018-01-12 21:01:55 -0500

    moreshit

--- src/App.js
+++ src/App.js
@@ -1,86 +1,124 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import {
+  Badge,
   Button,
   ButtonGroup,
-  Label,
-  Navbar,
+  Container,
+  Collapse,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
   Nav,
   NavItem,
-  NavDropdown,
-  MenuItem,
+  NavLink,
+  Navbar,
+  NavbarBrand,
+  NavbarToggler,
+  Row,
   Table,
-  Grid
-} from 'react-bootstrap';
-
+  UncontrolledDropdown,
+} from 'reactstrap';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.toggle = this.toggle.bind(this);
+    this.state = {
+      isOpen: false
+    };
+  }
+  toggle() {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  }
   render() {
     return (
       <div>
-
-        <Navbar>
-          <Navbar.Header>
-            <Navbar.Brand>
-              <a href="#home">Long Island Satsang</a>
-            </Navbar.Brand>
-          </Navbar.Header>
-          <Nav>
-            <NavItem eventKey={1} href="#">
-              Contact
-            </NavItem>
-          </Nav>
+        <Navbar color="primary" dark expand="md">
+          <NavbarBrand href="/">Long Island Area Satsang</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse isOpen={this.state.isOpen} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="#">Contact</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                    Login
+                </DropdownToggle>
+                <DropdownMenu >
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
         </Navbar>
+        <Container>
+        <br></br>
+        <Row><h4>Transmission Details</h4></Row>
+        <br></br>
 
-        <Grid>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Date</th>
-              <th>Connection Person</th>
-              <th>RSVP</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>1</td>
-              <td>January 20th, 2018</td>
-              <td>Agam Dua <Label bsStyle="warning">Not RSVP'd yet</Label></td>
-              <td>
-                <ButtonGroup bsSize="xsmall">
-                  <Button>Yes</Button>
-                  <Button>No </Button>
-                </ButtonGroup>
-              </td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>January 27th, 2018</td>
-              <td>Shetal Shah</td>
-              <td>
-                <ButtonGroup bsSize="xsmall">
-                  <Button bsStyle="success">Yes</Button>
-                  <Button>No</Button>
-                </ButtonGroup>
-              </td>
-            </tr>
-            <tr>
-              <td>1</td>
-              <td>February 3, 2018</td>
-              <td>Agam Dua <Label bsStyle="danger">Volunteer Declined!</Label></td>
-              <td>
-                <ButtonGroup bsSize="xsmall">
-                  <Button>Yes</Button>
-                  <Button bsStyle="danger" bsSize="xsmall">No</Button>
-                </ButtonGroup>
-              </td>
-            </tr>
-          </tbody>
-        </Table>
-        </Grid>
+          <Row>
+          <Table hover>
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Date</th>
+                <th>Connection Person</th>
+                <th>RSVP</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>January 20th, 2018</td>
+                <td>Agam Dua <Badge color="warning">Waiting for RSVP</Badge></td>
+                <td>
+                  <ButtonGroup size="sm">
+                    <Button>Yes</Button>
+                    <Button>No </Button>
+                  </ButtonGroup>
+                </td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>January 27th, 2018</td>
+                <td>Shetal Shah</td>
+                <td>
+                  <ButtonGroup size="sm">
+                    <Button color="success">Yes</Button>
+                    <Button>No</Button>
+                  </ButtonGroup>
+                </td>
+              </tr>
+              <tr>
+                <td>3</td>
+                <td>February 3, 2018</td>
+                <td>Agam Dua <Badge color="danger">Declined - Find Backup</Badge></td>
+                <td>
+                  <ButtonGroup size="sm">
+                    <Button>Yes</Button>
+                    <Button color="danger" size="sm">No</Button>
+                  </ButtonGroup>
+                </td>
+              </tr>
+            </tbody>
+          </Table>
+          </Row>
+        </Container>
       </div>
     );
   }
